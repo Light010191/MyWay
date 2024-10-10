8fa3520151e18cd3601a19596e0b134115011cbf
Light010191 <light010191@yandex.ru>
2024-10-10 23:05:03 +0300

    upd2

--- MyWay/Data/MyHelpers/FileUploadHelper.cs
+++ MyWay/Data/MyHelpers/FileUploadHelper.cs
@@ -2,19 +2,18 @@
 using System.IO;
 using System.Threading.Tasks;
 
-namespace BooksHub.Helpers
+namespace MyWay.Data.MyHelpers
 {
     public class FileUploadHelper
     {
         static public async Task<string> UploadAsync(Microsoft.AspNetCore.Http.IFormFile fromFile)
         {
             if (fromFile != null)
-            {               
+            {
                 var filename = $"{Guid.NewGuid()}{Path.GetExtension(fromFile.FileName)}";
                 using var fs = new FileStream(@$"wwwroot/uploads/img/{filename}", FileMode.Create);
                 await fromFile.CopyToAsync(fs);
                 return @$"/uploads/img/{filename}";
-
             }
 
             throw new Exception("File was not upload");
